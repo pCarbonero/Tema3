8a36537b65a5cc37fef696bf1e5f2df00a02f406
pCarbonero <pablo.carbonero@iesnervion.es>
2023-11-17 14:40:05 +0100

    [UPD]Ejercicio 01

---
+++ Tema3/src/ejerciciosfunciones/ejercicios01/Eco.java
@@ -0,0 +1,12 @@
+package ejerciciosfunciones.ejercicios01;
+
+
+public class Eco {
+	//creamos la funcion eco que recibe un int
+	public static void eco(int num) {
+		for (int i = 1; i <= num; i++) {
+			System.out.println("Eco");			
+		}
+	}
+
+}
--- Tema3/src/ejerciciosfunciones/ejercicios01/Main.java
+++ Tema3/src/ejerciciosfunciones/ejercicios01/Main.java
@@ -1,10 +1,24 @@
 package ejerciciosfunciones.ejercicios01;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// variables
-
+		// creamos la variable para guardar un número
+		int num;
+		//creamos el escaner
+		Scanner sc = new Scanner(System.in);
+		//preguntamos por el número
+		System.out.println("simnim");
+		//guardamos el número introducido n la variable num
+		num = sc.nextInt();
+		
+		//llamamos la funcion eco de la clase Eco y le pasamos el valor introducido a su variable int
+		Eco.eco(num);
+		
+		//cerramos el escaner
+		sc.close();
 	}
 
 }
