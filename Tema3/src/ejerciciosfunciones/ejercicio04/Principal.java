c9414f1eac2b06121c5d75326b6a81b2a149b118
pCarbonero <pablo.carbonero@iesnervion.es>
2023-11-22 09:49:29 +0100

    [UPD] ejercicio 4

--- Tema3/src/ejerciciosfunciones/ejercicio04/Principal.java
+++ Tema3/src/ejerciciosfunciones/ejercicio04/Principal.java
@@ -7,9 +7,23 @@ public class Principal {
 	public static void main(String[] args) {
 		// variable para guardar lo introducido
 		String in;
-		//escaner
+		// escaner
 		Scanner sc = new Scanner(System.in);
+		// pedimos al usuario que escriba po teclado
+		System.out.println("Escribe una letra");
+		// guardamos lo introducido en la variable in
+		in = sc.nextLine();
 
+		// lamamos a la funcion, si es true imprimimos que si es una vocal
+		if (Vocal.compVocal(in)) {
+			System.out.println("Es una vocal ");
+			// si no imprimimos lo conrtrario
+		} else {
+			System.out.println("No es una vocal ");
+
+		}
+		// cerramos el escaner
+		sc.close();
 	}
 
 }
--- Tema3/src/ejerciciosfunciones/ejercicio04/Vocal.java
+++ Tema3/src/ejerciciosfunciones/ejercicio04/Vocal.java
@@ -2,18 +2,24 @@ package ejerciciosfunciones.ejercicio04;
 
 public class Vocal {
 
+	// creamos la funcion bool que va a comprobar si el caracter es una vocal o no
 	public static boolean compVocal(String a) {
+		// creamos la variable que vamos a devolver
 		boolean isChar = false;
-		
+
+		// switch para comprobar si el valor es una vocal
 		switch (a) {
-		case "A","a","E","e","I","i","O","o","U","u": {
-			
+		// en casi de que sea una de esas opciones la bool es true
+		case "A", "a", "E", "e", "I", "i", "O", "o", "U", "u": {
+
 			isChar = true;
+			break;
 		}
+		// en cualquier otro caso es false
 		default:
 			isChar = false;
 		}
-		
+		// devolvemos el valor de la bool
 		return isChar;
 	}
 }
