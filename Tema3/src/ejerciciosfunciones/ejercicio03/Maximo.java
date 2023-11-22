b5a4ce27eb0e70df58bdbcbbfe23b188068bd7e7
pCarbonero <pablo.carbonero@iesnervion.es>
2023-11-22 09:04:26 +0100

    [new] ej 03

--- Tema3/src/ejerciciosfunciones/ejercicio03/Maximo.java
+++ Tema3/src/ejerciciosfunciones/ejercicio03/Maximo.java
@@ -5,9 +5,9 @@ public class Maximo {
 	//creamos la funcion que recibe dos parámetros
 	public static int max(int a, int b) {
 		int numMax = 0;
-		
+		//igualamos la varaible al numero mayor de los dos parámetros
 		numMax = (a > b) ? a : b;
-		
+		//devolvemos el numero mayor
 		return numMax;
 	}
 }
--- Tema3/src/ejerciciosfunciones/ejercicio03/Principal.java
+++ Tema3/src/ejerciciosfunciones/ejercicio03/Principal.java
@@ -1,14 +1,29 @@
 package ejerciciosfunciones.ejercicio03;
 
+import java.util.Scanner;
+
 public class Principal {
 
 	public static void main(String[] args) {
 		// variable para guardar los dos números enteros
-		int a, b;
+		int a, b, res;
 		
 		//variable para guardar el escaner 
-		//Scanner sc = new Scanner()
-
+		Scanner sc = new Scanner(System.in);
+		
+		//pedimos los números y los guardamos en ls variables
+		System.out.println("Dime el primer número ");
+		a = sc.nextInt();
+		System.out.println("Dime el segundo número ");
+		b = sc.nextInt();
+		
+		
+		//igualamos la variable resultado a al valor que devuelve la funcion
+		res = Maximo.max(a, b);
+		//imprimimos el resultado
+		System.out.println(res);
+		//cerramos el escaner
+		sc.close();
 	}
 
 }
