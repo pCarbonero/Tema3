dd4e4c7ec734d222be78ce08d21a70b3d4c60ee5
Propietario <Propietario@DESKTOP-I8MN8QL>
2023-12-06 19:13:04 +0100

    [new] ejercicio01 ampliacion

---
+++ Tema3/src/ampliacionfunciones/Numeros.java
@@ -0,0 +1,71 @@
+package ampliacionfunciones;
+
+public class Numeros {
+	// creamos un atributo para la clase número
+	int numero;
+
+	// creamos un constructor para la clase Numeros
+	public Numeros(int numero) {
+		// igualamos la variable numero de la clase al valor del parámetro del
+		// constructor
+		this.numero = numero;
+	}
+
+	// creamos una fucncion tipo boolean que nos devolverá si numero es primo o no
+	boolean esPrimo() {
+		boolean esP = true;
+		
+		// bucle for que comprobará si el número es primo en un rango de i entre 1 y numero
+		for (int i = 2; i < numero; i++) {
+			// comprobamos si el módulo de numero entre i es 0, en caso de serlo significa
+			// que el numero no es primo
+			if (numero % i == 0) {
+				esP = false;
+			}
+		} // fin bucle
+		
+		// si el número solo es divisible entre 1 y el mismo es primo, excepto si es 1,
+		// por lo que comprobamos is loes. En caso de serlo igualamos la booleana a
+		// false	
+		if (esP && numero == 1) {
+			esP = false;
+		}
+		
+		//devolvemos el valor de la booleana
+		return esP;
+	}
+	
+	
+	// creamos una funcion que comprueba si el número es capicúa
+	boolean esCapicua() {
+		// boolean para almacenar si numero es capicua o no
+		boolean esCapi = false;
+		// variable aux para igualarlo a numero, de esta forma podemos manipular el valor de numero sin modificar la variable original
+		int aux;
+		//int inverso en el cual almacenaremos el valor de numero al reves
+		int inverso = 0;
+		// variable para almacenar cual es la ultima cifra de aux
+		int ultimo;
+		
+		//igualamos aux a numero
+		aux = numero;
+		
+		//bucle para crear el valro de numero al revés
+		while(aux>=1) {
+			//igualamos ultimo al modulo de aux entre 10, que  es la ultima cifra de aux
+			ultimo = aux%10;
+			//igualamos inverso a inverso*10 + la ultimna cifra de aux, asi poco a poco iremos formando el numero invertido
+			inverso = inverso * 10 + ultimo;
+			//dividimos aux/10 de forma que en la proxima iteración aux tendrá una cufra menos
+			aux = aux/10;
+		}
+		
+		//comprobamos que numero es igual a inverso
+		if (inverso == numero) {
+			// en caso de serlo hacemos la booleana true
+			esCapi = true;
+		}
+		// devolvemos el valor de la booleana esCapi
+		return esCapi;
+	}
+}
---
+++ Tema3/src/ampliacionfunciones/Principal.java
@@ -0,0 +1,62 @@
+package ampliacionfunciones;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// variable para almacenar el numero introducido por el usuario
+		int numero = -1; //igualamos el numero a 0 para que el bucle do while funcione correctamente
+		// creamos un scanner para introducir un número solicitdao al usuario
+		Scanner sc = new Scanner(System.in);
+		//creamos booleanas para saber si es capicua y primo
+		boolean esPrimo, esCapicua;
+		//creamos una variable de la clase Numeros
+		Numeros num;
+		
+		//bucle para que si el usuario introduce un dato erroneo vuleva a pedirle un número
+		do {
+			try {
+				//preguntamos al usuario por un número entero
+				System.out.println("Dime un número entero ");
+				//guardamos el número dentro de la variable numero
+				numero = sc.nextInt();
+				//limpiamos el Scanner
+				sc.nextLine();
+			} catch (InputMismatchException e) {
+				// Imprimimnos un mesnaje de error
+				System.out.println("Valor introducido incorrecto ");
+			}
+		} while (numero < 0);
+		
+		//creamos un objeto de la clase Numeros	
+		num = new Numeros(numero);
+		// igualamos la variable esPrimo al valor devuelto de la funcion esPrimo de la funcion Numeros
+		esPrimo = num.esPrimo();
+		// igualamos la variable esCapicua al valor devuelto de la funcion esCapicua de la funcion Numeros
+		esCapicua = num.esCapicua();
+		
+		//si el numero es capicua imprimos un mensaje infromando al usuario de que sí lo es
+		if (esCapicua) {
+			System.out.println("El número introducido es capicúa ");
+		}
+		//si el numero es no capicua imprimos un mensaje infromando al usuario de que no lo es
+		else {
+			System.out.println("El número introducido no es capicúa ");
+		}
+		
+		//si el numero es primo imprimos un mensaje infromando al usuario de que sí lo es
+		if (esPrimo) {
+			System.out.println("El número introducido es primo ");
+		}
+		//si el numero es no primo imprimos un mensaje infromando al usuario de que no lo es
+		else {
+			System.out.println("El número introducido no es primo ");
+		}
+		
+		//cerramos el escaner
+		sc.close();
+	}//fin main
+
+}
