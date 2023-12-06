cd310ff09f455a6b0720f5af0818643b602f4e69
Propietario <Propietario@DESKTOP-I8MN8QL>
2023-12-06 13:28:21 +0100

    [new] ejercicio 3

--- Tema3/src/funcionesatributos/ejercicio03/Circulo.java
+++ Tema3/src/funcionesatributos/ejercicio03/Circulo.java
@@ -1,16 +1,21 @@
 package funcionesatributos.ejercicio03;
 
 public class Circulo {
+	//creamos la variable radio que la usaremos en las funciones
 	double radio;
 	
+	//creamos un constructor con un parámetro en el que almacenamos el radio
 	public Circulo(double radio) {
+		//igualamos la variable radio de la clase con la variable radio del constructor
 		this.radio = radio;
 	}
 	
+	//funcion para calcular el perímetro
 	double circunferencia() {
 		return 2*Math.PI*radio;
 	}
 	
+	//fuyncion para calcualr el área
 	double area() {
 		return Math.PI*Math.pow(radio, 2);
 	}
--- Tema3/src/funcionesatributos/ejercicio03/Principal.java
+++ Tema3/src/funcionesatributos/ejercicio03/Principal.java
@@ -1,5 +1,6 @@
 package funcionesatributos.ejercicio03;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Principal {
@@ -12,7 +13,7 @@ public class Principal {
 		double radio;
 		// creamos variable para guardar el resultado de las operaciones
 		double res;
-		
+		//creamos una variable de la clase círculo  para poder crear un objeeto de la clase
 		Circulo cl;
 		
 		//llamamos a la funcion pideRadio e igualamos el valor que devuelve con la variable radio
@@ -20,36 +21,70 @@ public class Principal {
 		//llamamos a la funcion del menu e igualamos el valor que devuelve con la variable opcion
 		opcion = menu();
 		
+		//creamos un objeto de la clase Círculo para llamar a sus funciones
 		cl = new Circulo(radio);
 		
 		switch (opcion) {
-		case 1: {
-			
+		case 1: {			
+			res = cl.circunferencia();
+			System.out.println("El perímetro es " + res);
+			break;
 		}
+		case 2: {
+			res = cl.area();
+			System.out.println("El área es " + res);
+			break;
+		}
+		
 		default:
-			throw new IllegalArgumentException("Unexpected value: " + opcion);
-		}
+			System.out.println("Has salido ");
+		};
+		
+		
 
-	}
+	}//fin main
 	
 	//funcion menu() que imprime el menu y devuelve la opcion elegida
 	public static int menu() {
-		int opc;
-		System.out.println("MENU");
-		System.out.println("1. Perímetro\r\n"
-				+ "2. Área\r\n"
-				+ "0. Salir\r\n"
-				+ "");
-		opc = sc.nextInt();
+		int opc =-1;
+		
+		//bucle do while para que si el valor intoducido no está entre ls opciones el usuario tenga que volver a introducirlo
+		do {
+			try {
+				System.out.println("MENU");
+				System.out.println("1. Perímetro\r\n"
+						+ "2. Área\r\n"
+						+ "0. Salir\r\n"
+						+ "");
+				opc = sc.nextInt();
+				sc.nextLine();
+			} catch (InputMismatchException e) {
+				// Inprimimos un mensaje de error
+				System.out.println("Valor incorrecto ");
+				sc.nextLine();
+			}
+			
+		} while (opc < 0 || opc > 3);
+
 		return opc;
 	}
 	
 	//funcion pide radio que pide el radio y devuelve el valor de este
 	public static double pideRadio() {
-		double radio;
-		System.out.println("Dime el radio de una circunferencia ");
-		radio = sc.nextDouble();
+		double radio = -1;
+		
+		//bucle do while para que si el valor intoducido no está entre ls opciones el usuario tenga que volver a introducirlo
+		do {
+			try {
+				System.out.println("Dime el radio de una circunferencia ");
+				radio = sc.nextDouble();
+			} catch (InputMismatchException e) {
+				// Inprimimos un mensaje de error
+				System.out.println("Valor incorrecto ");
+				sc.nextLine();
+			}
+		} while (radio<0);
 		return radio;
-	}
+	}//fin pideRadio
 
 }
