f1224d13c5194c71125b6f361ac639863b941de2
Propietario <Propietario@DESKTOP-I8MN8QL>
2023-12-07 19:27:07 +0100

    [upd] fin ejercicio05 ampliación

--- Tema3/src/ampliacionfunciones/ejercicio05/Figuras.java
+++ Tema3/src/ampliacionfunciones/ejercicio05/Figuras.java
@@ -16,26 +16,59 @@ public class Figuras {
 	
 	//creamos una función que imprimirá el triñángulo equilatero
 	void triangulo() {
+		// hacemos un bucle for que hará el valor de lado en saltos de línea
 		for (int i = 1; i <= lado; i++) {
+			// bucle que imprime el número de espacios antes de dibujar el primer caracter
 			for (int j = 1; j <= lado-i; j++) {
 				System.out.print(" ");
 				
 			}
+			// bucle que dibuja los caracteres
 			for (int j = 1; j <= i; j++) {
 				System.out.print(caracter + " ");
 				
 			}
+			// salto de línea
 			System.out.println();
 		}//fin for 1
 	}//fin triangulo
 	
+	// creamos una función que dibujará el cuadrado
 	void cuadrado() {
+		// hacemos un bucle for que hará el valor de lado en saltos de línea 
 		for (int i = 1; i <= lado; i++) {
+			// bucle que dibuja los mismos caracteres que el valro de lado
 			for (int j = 1; j <= lado; j++) {
+				// dibuja un caracter
 				System.out.print(caracter);			
 			}
+			// salto de linea
 			System.out.println();
 		}
 	}
 	
-}
+	void hexagono() {
+		for (int i = 0; i <= lado-1; i++) {
+			
+			for (int j = 2; j <= lado-i; j++) {
+				System.out.print(" ");			
+			}
+			for (int j = 1; j <= lado+(i*2); j++) {
+				System.out.print(caracter);			
+			}
+			System.out.println();
+			}//fin for 1
+		
+		for (int i = 0; i <	lado-1; i++) {
+			for (int j = lado; j >= lado-i; j--) {
+				System.out.print(" ");		
+			}
+			for (int j = 1; j <= lado+((lado-2)*2)-(i*2); j++) {
+				System.out.print(caracter);			
+			}
+			System.out.println();
+		}
+		
+		}//fin hexagono()
+	}
+
--- Tema3/src/ampliacionfunciones/ejercicio05/Principal.java
+++ Tema3/src/ampliacionfunciones/ejercicio05/Principal.java
@@ -1,21 +1,137 @@
 package ampliacionfunciones.ejercicio05;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Principal {
 
-	public static void main(String[] args) {
-		// variables
-		int lado;
-		char caracter;
-		Figuras fig; 
-		
-		lado = 4;
-		caracter = '*';
-		
-		fig = new Figuras(lado, caracter);
-		
-		fig.cuadrado();
-		
+	// creamos una variable static del scanner para poder usarlo en cualquier lugar
+	// de la clase
+	static Scanner sc = new Scanner(System.in);
 
+	public static void main(String[] args) {
+		// creamos una variable para guardar la longitud del lado
+		int lado = 0;
+		// creamos una variable para guardar el carácter con el que se dibujará la
+		// figura
+		char caracter = ' ';
+		// creamos una variable para guardar el valor de la opcion
+		int opcion;
+		// creamos una variable de la clase Figuras para más tarde poder crear un objeto
+		Figuras fig;
+
+		// llamamos a la función pideLado() e igualamos la variable lado al valor que
+		// devuelve
+		lado = pideLado();
+
+		// llamamos a la función pideCaracter() e igualamos la variable caracter al
+		// valor que devuelve
+		caracter = pideCaracter();
+		
+		// llamamos al menú e igualamos la variable opcion al valor que devuelve
+		opcion = menu();
+
+
+		// creamos un objeto de la clase para poder acceder a las funciones de la clase
+		fig = new Figuras(lado, caracter);
+
+		// creamos un switch que dependoiendo del valor de option hará una cosa
+		switch (opcion) {
+		case 1: {
+			fig.triangulo();
+			break;
+		}
+		case 2: {
+			fig.cuadrado();
+			break;
+		}
+		case 3: {
+			fig.hexagono();
+			break;
+		}
+
+		default:
+			System.out.println("Has salido ");
+		}
+		;
+
+	}
+
+	public static int menu() {
+		// creamos la variable que va a almacenar el valor de la opción elegida
+		int option = -1; // se iguala a -1 para que el bucle do while funcione correctamente
+
+		// bucle que siempre que el valor de option sea menor de 0 o mayor de 3(es decir
+		// que no se ha intorudcido una opcion correcta) se repita
+		do {
+			// creamos un try catch para preguntar por la opción y para imprimir mensaje de
+			// error si es necesario
+			try {
+				// imprimimos el menú
+				System.out.println("MENU");
+				System.out.println("1. Triángulo\r\n" + "2. Cuadrado\r\n" + "3. Hexágono\r\n" + "0. Hexágono\r\n" + "");
+				// guardamos el valor introducido en la variable option
+				option = sc.nextInt();
+				// Limpiamos el escaner
+				sc.nextLine();
+
+			} catch (InputMismatchException e) {
+				// Imprimimos mensaje de error
+				System.out.println("Valor introducido incorrecto ");
+				// Limpiamos el escaner
+				sc.nextLine();
+			} // catch
+		} while (option < 0 || option > 4);
+
+		// devolvemos el valor con la que acabe la funcion la variable option
+		return option;
+	}// fin menu()
+
+	// creamos una función que nos pedirá el lado de la figura
+	public static int pideLado() {
+		int lado = 0;
+
+		// bucle que si el usuario no introduce un valor válido se repetirá
+		do {
+			// try catch por si el usuario introduce un valor qeu no es de tipo int
+			try {
+				// preguntamos al usuario por la longitud del lado
+				System.out.println("Cuánto mide el lado? ");
+				lado = sc.nextInt();
+				// limìamos el scanner
+				sc.nextLine();
+			} catch (InputMismatchException e) {
+				// Imprimimos mensaje de error
+				System.out.println("Valor incorrecto ");
+				// limìamos el scanner
+				sc.nextLine();
+			}
+		} while (lado <= 1);
+		// devolvemos el valor de lado
+		return lado;
+	}// fin pideLado
+
+	// creamos una función que nos pedirá con que carácter quiere que dibujemos la
+	// figura
+	public static char pideCaracter() {
+		char caracter = ' ';
+
+		// // bucle que si el usuario no introduce un valor válido se repetirá
+		do {
+			try {
+				System.out.println("Qué carácter quieres utilizar para la figura? ");
+				caracter = sc.next().charAt(0);
+				// limìamos el scanner
+				sc.nextLine();
+			} catch (InputMismatchException e) {
+				// Imprimimos mensaje de error
+				System.out.println("Valor incorrecto ");
+				// limìamos el scanner
+				sc.nextLine();
+			}
+		} while (caracter == ' ');
+
+		return caracter;
 	}
 
 }
